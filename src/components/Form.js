c15bd3bd824c8094b762254690b5f24d341e005b
phunky-panda94 <victor.nim94@outlook.com>
2021-10-25 12:22:53 +1100

    styled form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,14 +2,32 @@ import InputField from './InputField';
 
 function Form(props) {
     return (
-        <div className="form flex flex-jc-c ">
-            <div className="form-input flex flex-col flex-jc-se">
-                <h3>{props.heading}</h3>
+        <div className="form flex flex-col flex-jc-c flex-ai-c">
+            <form className="form-input flex flex-col flex-jc-sb flex-ai-c">
+                <FormHeading text={props.heading} />
                 <InputField type="text" label={props.org} />
                 <InputField type="text" label={props.title} />
                 <InputField type="date" label={props.startDate} />
                 <InputField type="date" label={props.endDate} />
-            </div>
+                <FormButtons />
+            </form>
+        </div>
+    )
+}
+
+function FormHeading(props) {
+    return (
+        <div className="form-div flex flex-jc-c">
+            <div className="form-heading">{props.text}</div>
+        </div>
+    )
+}
+
+function FormButtons() {
+    return (
+        <div className="form-div flex flex-jc-fe">
+            <button type="submit">Add</button>
+            <button type="button">Discard</button>
         </div>
     )
 }
